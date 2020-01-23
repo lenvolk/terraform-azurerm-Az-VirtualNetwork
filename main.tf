d041550e17704cce906758e94f343c8a0e39ddf8
lenvolk <lvolkov@outlook.com>
2020-01-23 15:19:19 +0000

    commneted udr in subnets

--- main.tf
+++ main.tf
@@ -59,7 +59,7 @@ resource "azurerm_subnet" "subnets" {
   resource_group_name       = data.azurerm_resource_group.network.name
   address_prefix            = each.value["address_prefix"]
   service_endpoints         = lookup(each.value, "service_endpoints", null)
-  route_table_id            = lookup(each.value, "rt_key", null) == null ? null : lookup(azurerm_route_table.rts, each.value["rt_key"], null)["id"]
+  #route_table_id            = lookup(each.value, "rt_key", null) == null ? null : lookup(azurerm_route_table.rts, each.value["rt_key"], null)["id"]
   #network_security_group_id = lookup(each.value, "nsg_key", null) == null ? null : lookup(azurerm_network_security_group.nsgs, each.value["nsg_key"], null)["id"]
 
   /*
